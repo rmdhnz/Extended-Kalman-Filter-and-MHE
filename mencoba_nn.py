7174b65baad85f91a74e333bf2fe76571f703e8f
lazuardiff <lazuardifazari@gmail.com>
2024-06-20 16:03:19 +0700

    pull

--- mencoba_nn.py
+++ mencoba_nn.py
@@ -3,38 +3,45 @@ from tes import data
 import numpy as np
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
-flow_csv = data["Flow Measured Value"]
-set_point_csv = data["Flow Set Point"]
-waktu = data["Time"]
-# flow = np.array(flow_csv)
-# set_point = np.array(set_point_csv)
-flow = np.reshape(flow_csv,(1,554))
-set_point = np.reshape(set_point_csv,(1,554))
+import pandas as pd
+# flow_csv = data["Flow Measured Value"]
+# set_point_csv = data["Flow Set Point"]
+# waktu = data["Time"]
+# # flow = np.array(flow_csv)
+# # set_point = np.array(set_point_csv)
+# flow = np.reshape(flow_csv,(1,554))
+# set_point = np.reshape(set_point_csv,(1,554))
+
+data_test = pd.read_csv("data/data-setpoint-modifikasi.csv")
+set_point_csv_test = data_test["Flow Set Point"]
+waktu_csv_test = data_test["Time"]
+flow_csv_test = data_test["Flow Measured Value"]
+print(set_point_csv_test)
 # Instantiate MLPRegressor
 
-nn = MLPRegressor(
-    activation='relu',
-    hidden_layer_sizes=(100, 100),
-    alpha=0.001,
-    random_state=20,
-    early_stopping=False
-)
+# nn = MLPRegressor(
+#     activation='relu',
+#     hidden_layer_sizes=(100, 100),
+#     alpha=0.001,
+#     random_state=20,
+#     early_stopping=False
+# )
 
-nn.fit(set_point, flow)
-pred = nn.predict(set_point)
-#
-# Calculate accuracy and error metrics
-#
-test_set_rmse = np.sqrt(mean_squared_error(flow, pred))
-#
-# Print R_squared and RMSE value
-#
-print('RMSE: ', test_set_rmse)
-pred = np.reshape(pred,(554,1))
+# nn.fit(set_point, flow)
+# pred = nn.predict(set_point)
+# #
+# # Calculate accuracy and error metrics
+# #
+# test_set_rmse = np.sqrt(mean_squared_error(flow, pred))
+# #
+# # Print R_squared and RMSE value
+# #
+# print('RMSE: ', test_set_rmse)
+# pred = np.reshape(pred,(554,1))
 
-plt.plot(waktu,set_point_csv,label="set point",color="orange",linestyle="dotted")
-plt.plot(waktu,flow_csv,label="flow",color="red")
-plt.plot(waktu,pred,label="prediksi",color="blue")
+plt.plot(waktu_csv_test,set_point_csv_test,label="set point",color="orange",linestyle="dotted")
+plt.plot(waktu_csv_test,flow_csv_test,label="flow",color="red")
+# plt.plot(waktu,pred,label="prediksi",color="blue")
 plt.legend()
 plt.grid(True)
 plt.show()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+# flow = np.array(flow_csv)
+# set_point = np.array(set_point_csv)
